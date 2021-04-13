6ba124982eb4822048deb03ebd245a52a1b2caa4
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2021-04-13 16:29:15 +0000

    [Minor Update on env_setup] Issue01

--- env_setup.sh
+++ env_setup.sh
@@ -6,7 +6,7 @@
 #    By: mazoukni <mazoukni@student.1337.ma>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/04/13 13:45:20 by mazoukni          #+#    #+#              #
-#    Updated: 2021/04/13 16:08:51 by mazoukni         ###   ########.fr        #
+#    Updated: 2021/04/13 16:16:06 by mazoukni         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,29 +16,32 @@ DOCKER_HOME=$HOME/.docker
 DOCKER_GOINFRE=/goinfre/$1/.docker
 HOMEBREW_HOME=/goinfre/$1/homebrew
 
-if [ ! -d ~/.zshrc ]
+if [ ! -d "~/.zshrc" ]
 then
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-echo -n "" > ~/.zshrc
+echo " " > ~/.zshrc
 echo "export ZSH=\"/Users/$1/.oh-my-zsh\"" >> ~/.zshrc
 echo "ZSH_THEME=\"robbyrussell\"" >> ~/.zshrc
 echo "plugins=(git)"  >> ~/.zshrc
 echo "source $ZSH/oh-my-zsh.sh" >> ~/.zshrc
+source ~/.zshrc
 
-if [[ -d $DOCKER_GOINFRE && -d $HOMEBREW_HOME ]]
+if [[ -d "/goinfre/$1/.docker" && -d "/goinfre/$1/homebrew" ]]
 then
     rm -rf $DOCKER_GOINFRE
     rm -rf $HOMEBREW_HOME
 fi
 mkdir $HOMEBREW_HOME && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOMEBREW_HOME
 echo "export PATH=$PATH:$HOMEBREW_HOME/bin" >> ~/.zshrc
+source ~/.zshrc
 curl -Lo /goinfre/$1/minikube https://storage.googleapis.com/minikube/releases/v0.27.0/minikube-darwin-amd64
 chmod +x /goinfre/$1/minikube
 echo "export PATH=$PATH:/goinfre/$1" >> ~/.zshrc
 echo "export MINIKUBE_HOME=/goinfre/$1" >> ~/.zshrc
 echo "export KUBECONFIG=/goinfre/$1/.kube/config"
+source ~/.zshrc
 brew update
 brew install docker docker-machine
 mkdir $DOCKER_GOINFRE
@@ -48,7 +51,7 @@ docker-machine start
 docker-machine env default
 eval "$(docker-machine env default)"
 brew install kubectl
-if [ -d $HOME/.minikube ]
+if [ -d "$HOME/.minikube" ]
 then
     rm -rf $HOME/.minikube
 fi
